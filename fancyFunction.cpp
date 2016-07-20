3e4110f982af25b30a0083b027a06f59c1f54ddf
Lukas Reimer <Luki@Lukis-MBP.home>
2016-07-20 11:54:56 +0200

    first implementation of add plus additional stringify method for controlling precision

--- fancyFunction.cpp
+++ fancyFunction.cpp
@@ -5,6 +5,7 @@
 #include "fancyFunction.h"
 
 #include <fstream>
+#include <iomanip>
 #include <string>
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
@@ -15,33 +16,46 @@ template<class T>
 class Database
 {
     private:
-        fs::path db_root = "./.db/";
-    public:
-        T retrieve( std::vector<T> const & v)
+        fs::path db_root = "./.db/";  // root folder for database tree
+        int precision = 10;  // decimal precision for path and data variables
+        std::string stringify(T const & val)
+        {
+            std::stringstream ss;
+            ss << std::setprecision(precision) << std::fixed << val;
+            return ss.str();
+        }
+        fs::path vectorToPath(std::vector<T> const & vec)
         {
             // transform vector to path:
             fs::path index_path = db_root;
-            for(size_t i = 0; i != v.size(); i++) {
+            for(size_t i = 0; i != vec.size(); i++) {
                 //std::cout << v.at(i);  // debug output
-                if (i == v.size() - 1) {  // last element (leaf node)
+                if (i == vec.size() - 1) {  // last element (leaf node)
                     //std::cout << ", leaf element." << std::endl;  // debug output
-                    index_path.append(std::to_string(v.at(i)) + ".leaf");
+                    index_path.append(stringify(vec.at(i)) + ".leaf");
                 }
                 else {  // all intermediate elements (path nodes)
                     //std::cout << ", path element." << std::endl;  // debug output
-                    index_path.append(std::to_string(v.at(i)));
+                    index_path.append(stringify(vec.at(i)));
                 }
             }
             std::cout << "search path: " << index_path << std::endl;  // debug output
+            return index_path;
+        }
+    public:
+        T retrieve( std::vector<T> const & vec)
+        {
+            // transform vector to path:
+            fs::path search_path = vectorToPath(vec);
 
             // check if path exists and retrieve data:
-            bool existing = fs::exists(index_path);
+            bool existing = fs::exists(search_path);
             if (existing) {
                 //std::cout << "existing" << std::endl;  // debug output
                 double value;
                 // open and read file
                 std::ifstream infile;
-                infile.open(index_path);
+                infile.open(search_path);
                 if (infile.is_open()) {
                     // read first line and convert to double
                     std::string first_line;
@@ -62,11 +76,32 @@ class Database
             }
         }
 
-        void add( std::vector<T> const &, T const & )
+        void add( std::vector<T> const & vec, T const & val)
         {
-            // transform vector to path
-            // find out if path already exists
-            // add folderstructure and data file
+            // transform vector to path:
+            fs::path search_path = vectorToPath(vec);
+
+            // find out if path already exists:
+            bool existing = fs::exists(search_path);
+            if (existing) {
+                throw std::logic_error("path already exists!");
+            }
+            else {
+                // create folder structure and write data file:
+                //std::cout << "parent_path: " << search_path.parent_path() << std::endl;  // debug output
+                //std::cout << "search_path: " << search_path << std::endl;  // debug output
+                fs::create_directories(search_path.parent_path());
+                std::ofstream outfile;
+                outfile.open(search_path);
+                if (outfile.is_open()) {
+                    //std::cout << "writing data..." << std::endl;  // debug output
+                    outfile << std::setprecision(precision) << std::fixed
+                            << val << std::endl;
+                } else {
+                    throw std::ios_base::failure("unable to open file!");
+                }
+                outfile.close();
+            }
         }
 
         void dump()
@@ -74,7 +109,6 @@ class Database
             // create map from folder structure
             // write map to file
         };
-        /// ??
 };
 
 int main( int argc, char ** argv )
