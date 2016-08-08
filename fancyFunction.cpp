0a066b9081b474ec6197317cfe3f58d2aca37018
Lukas Reimer <Luki@Lukis-MBP.local>
2016-08-08 10:16:54 +0200

    code markdown in documentation

--- fancyFunction.cpp
+++ fancyFunction.cpp
@@ -64,6 +64,7 @@ namespace fs = std::experimental::filesystem;
  * the data that is stored in the database.
  *
  * example path: `.db/1.000000/2.000000/5.000000/3.000000.leaf`
+ *
  * example data in `.../3.000000.leaf`: `30.000000`
  *
  * A dump of the entire database is created in a file called "dump.db".
